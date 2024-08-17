18838b94ad6a062af7bc9090b96741b483d49912
till9527 <156125590+till9527@users.noreply.github.com>
2024-08-16 22:40:14 -0400

    Update OrderInformation.tsx

--- src/email/components/OrderInformation.tsx
+++ src/email/components/OrderInformation.tsx
@@ -50,22 +50,11 @@ export function OrderInformation({
         </Row>
       </Section>
       <Section className="border border-solid border-gray-500 rounded-lg p-4 md:p-6 my-4">
-        <Img
-          width="100%"
-          alt={product.name}
-          src={`${process.env.NEXT_PUBLIC_SERVER_URL}${product.imagePath}`}
-        />
         <Row className="mt-8">
           <Column className="align-bottom">
             <Text className="text-lg font-bold m-0 mr-4">{product.name}</Text>
           </Column>
           <Column align="right">
-            <Button
-              href={`${process.env.NEXT_PUBLIC_SERVER_URL}/products/download/${downloadVerificationId}`}
-              className="bg-black text-white px-6 py-4 rounded text-lg"
-            >
-              Download
-            </Button>
           </Column>
         </Row>
         <Row>
