c6edde24e9b4de92ee5d5ad623aef3383fcca0ae
till9527 <156125590+till9527@users.noreply.github.com>
2024-08-16 21:05:38 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {images: {
     domains: [
-      'e-commerce-official-bucket.s3.us-east-2.amazonaws.com  ',
+      'e-commerce-official-bucket.s3.us-east-2.amazonaws.com',
     ]
   }};
 
