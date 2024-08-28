9320cd4e41ec37bfdd0df5c85ba1243417433c52
till9527 <156125590+till9527@users.noreply.github.com>
2024-08-28 12:22:57 -0400

    Update PaginationControls.tsx

--- src/components/PaginationControls.tsx
+++ src/components/PaginationControls.tsx
@@ -1,5 +1,3 @@
-// components/PaginationControls.tsx
-
 "use client"; // This directive makes this a client component
 
 import { useRouter } from 'next/navigation';
@@ -14,7 +12,7 @@ import {
 
 const ITEMS_PER_PAGE = 6;
 
-export default function PaginationControls({ currentPage }: { currentPage: number }) {
+export default function PaginationControls({ currentPage, totalPages }: { currentPage: number, totalPages: number }) {
   const router = useRouter();
 
   const handlePagination = (page: number) => {
@@ -22,27 +20,37 @@ export default function PaginationControls({ currentPage }: { currentPage: numbe
   };
 
   return (
-    <Pagination>
-      <PaginationContent>
-        <PaginationItem>
-          <PaginationPrevious
-            href="#"
-            onClick={() => handlePagination(currentPage > 1 ? currentPage - 1 : 1)}
-            className={currentPage === 1 ? "cursor-not-allowed opacity-50" : ""}
-          />
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#" onClick={() => handlePagination(1)}>
-            1
-          </PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationNext
-            href="#"
-            onClick={() => handlePagination(currentPage + 1)}
-          />
-        </PaginationItem>
-      </PaginationContent>
-    </Pagination>
+    <div className="flex justify-center mt-8 mb-4">
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious
+              href="#"
+              onClick={() => handlePagination(currentPage > 1 ? currentPage - 1 : 1)}
+              className={currentPage === 1 ? "cursor-not-allowed opacity-50" : ""}
+              disabled={currentPage === 1}
+            />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#" onClick={() => handlePagination(currentPage)} className="px-4">
+              {currentPage}
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#" onClick={() => handlePagination(totalPages)} className="px-4">
+              / {totalPages}
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext
+              href="#"
+              onClick={() => handlePagination(currentPage < totalPages ? currentPage + 1 : totalPages)}
+              className={currentPage === totalPages ? "cursor-not-allowed opacity-50" : ""}
+              disabled={currentPage === totalPages}
+            />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    </div>
   );
 }
