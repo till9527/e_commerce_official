cb94451faeecc71a36e265a1ebe0b1e2d57ca58e
till9527 <156125590+till9527@users.noreply.github.com>
2024-08-28 12:37:51 -0400

    Update page.tsx

--- src/app/(customerFacing)/products/page.tsx
+++ src/app/(customerFacing)/products/page.tsx
@@ -22,14 +22,15 @@ export default async function ProductsPage({
   });
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {products.map((product) => (
-        <ProductCard key={product.id} {...product} />
-      ))}
-    </div>
-    <div className="flex justify-center mt-8 mb-4">
-      <PaginationControls currentPage={page} />
+    <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {products.map((product) => (
+          <ProductCard key={product.id} {...product} />
+        ))}
       </div>
+      <div className="flex justify-center mt-8 mb-4">
+        <PaginationControls currentPage={page} />
+      </div>
+    </div>
   );
 }
-
