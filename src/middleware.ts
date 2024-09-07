43cb4c299498af6e08799db939d01b5d66996b49
till9527 <156125590+till9527@users.noreply.github.com>
2024-09-07 09:20:21 -0400

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,34 +1,32 @@
-import { NextRequest, NextResponse } from "next/server"
-import { isValidPassword } from "./lib/isValidPassword"
+import { NextRequest, NextResponse } from "next/server";
+import { isValidPassword } from "./lib/isValidPassword";
 
 export async function middleware(req: NextRequest) {
-  if ((await isAuthenticated(req)) === false) {
+  if (!(await isAuthenticated(req))) {
     return new NextResponse("Unauthorized", {
       status: 401,
       headers: { "WWW-Authenticate": "Basic" },
-    })
+    });
   }
 }
 
 async function isAuthenticated(req: NextRequest) {
   const authHeader =
-    req.headers.get("authorization") || req.headers.get("Authorization")
+    req.headers.get("authorization") || req.headers.get("Authorization");
 
-  if (authHeader == null) return false
+  if (!authHeader) return false;
 
   const [username, password] = Buffer.from(authHeader.split(" ")[1], "base64")
     .toString()
-    .split(":")
+    .split(":");
 
+  // Assuming process.env.ADMIN_USERNAME is still used for the username
   return (
     username === process.env.ADMIN_USERNAME &&
-    (await isValidPassword(
-      password,
-      process.env.HASHED_ADMIN_PASSWORD as string
-    ))
-  )
+    (await isValidPassword(password))
+  );
 }
 
 export const config = {
   matcher: "/admin/:path*",
-}
+};
