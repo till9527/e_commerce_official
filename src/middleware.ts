246628167bff016c6d1b2ae9df0c945a3956bb58
till9527 <156125590+till9527@users.noreply.github.com>
2024-09-07 18:55:11 -0400

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,6 +19,7 @@ export async function middleware(req: NextRequest) {
   }
 
   if ((await isAuthenticated(req)) === false) {
+    console.log(`its false`);
     return new NextResponse("Unauthorized", {
       status: 401,
       headers: { "WWW-Authenticate": "Basic" },
