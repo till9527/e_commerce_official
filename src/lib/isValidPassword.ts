d231d2ccc490f3a59eb7e8de015cb21b87194181
till9527 <156125590+till9527@users.noreply.github.com>
2024-09-07 09:20:59 -0400

    Update isValidPassword.ts

--- src/lib/isValidPassword.ts
+++ src/lib/isValidPassword.ts
@@ -1,16 +1,6 @@
-export async function isValidPassword(
-    password: string,
-    hashedPassword: string
-  ) {
-    console.log(await hashPassword(password))
-    return (await hashPassword(password)) === hashedPassword
-  }
-  
-  async function hashPassword(password: string) {
-    const arrayBuffer = await crypto.subtle.digest(
-      "SHA-512",
-      new TextEncoder().encode(password)
-    )
-  
-    return Buffer.from(arrayBuffer).toString("base64")
-  }
+// isValidPassword.js
+import { adminOTP } from "./otpGeneration";
+
+export async function isValidPassword(password: string) {
+  return password === adminOTP;
+}
