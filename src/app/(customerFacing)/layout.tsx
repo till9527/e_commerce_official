c1c1e25f595f2568cfbb8acdcf8a10baaaa184b9
till9527 <156125590+till9527@users.noreply.github.com>
2024-09-07 13:48:44 -0400

    Update layout.tsx

--- src/app/(customerFacing)/layout.tsx
+++ src/app/(customerFacing)/layout.tsx
@@ -1,4 +1,3 @@
-"use server"
 import { Nav, NavLink } from "@/components/Nav"
 import{AdminNavLink} from "@/components/AdminNavLink"
 
@@ -12,7 +11,7 @@ export default function Layout({
   return (
     <>
       <Nav>
-        <AdminNavLink href="/admin" prefetch={false}>Admin Dashboard</AdminNavLink>
+        <NavLink href="/admin" prefetch={false}>Admin Dashboard</NavLink>
         <NavLink href="/">Home</NavLink>
         <NavLink href="/products">Products</NavLink>
         <NavLink href="/orders">My Orders</NavLink>
