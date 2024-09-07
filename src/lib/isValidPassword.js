48e3a22a8e5389a625b11b7930433c00c0405e6f
till9527 <156125590+till9527@users.noreply.github.com>
2024-09-07 10:11:45 -0400

    Update isValidPassword.js

--- src/lib/isValidPassword.js
+++ src/lib/isValidPassword.js
@@ -1,8 +1,16 @@
-// isValidPassword.js
-const { adminOTP } = require("./otpGeneration");
-
-
-async function isValidPassword(password) {
-  return password === adminOTP;
-}
-module.exports = { isValidPassword };
+export async function isValidPassword(
+    password: string,
+    hashedPassword: string
+  ) {
+    console.log(await hashPassword(password))
+    return (await hashPassword(password)) === hashedPassword
+  }
+  
+  async function hashPassword(password: string) {
+    const arrayBuffer = await crypto.subtle.digest(
+      "SHA-512",
+      new TextEncoder().encode(password)
+    )
+  
+    return Buffer.from(arrayBuffer).toString("base64")
+  }
