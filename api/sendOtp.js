a93a396df765bdb73cb7b44afee3ea195efbfd23
till9527 <156125590+till9527@users.noreply.github.com>
2024-09-07 17:49:28 -0400

    Update sendOtp.js

--- api/sendOtp.js
+++ api/sendOtp.js
@@ -4,7 +4,8 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     const { email } = req.body;
     try {
-      await sendOTP(email);
+      const adminOtp = await sendOTP(email);
+      res.setHeader('x-otp', adminOtp);
       res.status(200).json({ success: true, message: 'OTP sent successfully' });
     } catch (error) {
       res.status(500).json({ success: false, message: error.message });
