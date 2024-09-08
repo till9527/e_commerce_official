c642d728050f85dba664e007de74a9152a017aab
till9527 <156125590+till9527@users.noreply.github.com>
2024-09-08 09:16:40 -0400

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,34 +1,53 @@
-import { NextRequest, NextResponse } from "next/server"
-import { isValidPassword } from "./lib/isValidPassword"
+import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
+
+
   if ((await isAuthenticated(req)) === false) {
     return new NextResponse("Unauthorized", {
       status: 401,
       headers: { "WWW-Authenticate": "Basic" },
-    })
+    });
   }
 }
 
 async function isAuthenticated(req: NextRequest) {
-  const authHeader =
-    req.headers.get("authorization") || req.headers.get("Authorization")
+  const email = process.env.GMAIL_USER;
+  
+  // Send OTP
+  const response1 = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/sendOtp`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email }),
+  });
+ const result1 = await response1.json();
+ const adminOtp = response1.headers.get('x-otp');
+ console.log(`Got admin otp as ${adminOtp}`);
 
-  if (authHeader == null) return false
+  // Wait for 5 seconds before verifying OTP
+ 
+  const authHeader =
+    req.headers.get("authorization") || req.headers.get("Authorization");
+
+  if (authHeader == null) return false;
 
   const [username, password] = Buffer.from(authHeader.split(" ")[1], "base64")
     .toString()
-    .split(":")
+    .split(":");
+  const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/verifyOtp`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email: email, otpInput: password, adminOtp:adminOtp }),
+  });
 
-  return (
-    username === process.env.ADMIN_USERNAME &&
-    (await isValidPassword(
-      password,
-      process.env.HASHED_ADMIN_PASSWORD as string
-    ))
-  )
+  const result = await response.json();
+  return result.success;
 }
 
 export const config = {
   matcher: "/admin/:path*",
-}
+};
