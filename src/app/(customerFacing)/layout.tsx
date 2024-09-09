ea58e7fcc35e55057c58ce5539917dc8fad5491a
AaronDee13 <151183007+AaronDee13@users.noreply.github.com>
2024-09-09 19:57:41 -0400

    Update layout.tsx

--- src/app/(customerFacing)/layout.tsx
+++ src/app/(customerFacing)/layout.tsx
@@ -9,16 +9,12 @@ export default function Layout({
 }>) {
   return (
     <>
-      <div className="flex justify-center"> 
-        <div className = "flex gap-4 items-center max-w-3xl w-full justify-between px-4 py-3">
           <Nav>
             <NavLink href="/admin" prefetch={false}>Admin Dashboard</NavLink>
             <NavLink href="/">Home</NavLink>
             <NavLink href="/products">Products</NavLink>
             <NavLink href="/orders">My Orders</NavLink>
           </Nav>
-        </div>
-      </div>
       <div className="container my-6">{children}</div>
     </>
   )
