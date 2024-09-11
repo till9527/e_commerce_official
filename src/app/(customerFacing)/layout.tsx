57b532726e9b55a5bcd88209e033d05556a4cb45
till9527 <156125590+till9527@users.noreply.github.com>
2024-09-11 13:31:43 -0400

    Update layout.tsx

--- src/app/(customerFacing)/layout.tsx
+++ src/app/(customerFacing)/layout.tsx
@@ -1,5 +1,5 @@
 import { Nav, NavLink } from "@/components/Nav"
-import { Button } from "@/components/ui/button"
+
 export const dynamic = "force-dynamic"
 
 export default function Layout({
@@ -9,12 +9,12 @@ export default function Layout({
 }>) {
   return (
     <>
-          <Nav>
-            <NavLink href="/admin" prefetch={false}>Admin Dashboard</NavLink>
-            <NavLink href="/">Home</NavLink>
-            <NavLink href="/products">Products</NavLink>
-            <NavLink href="/orders">My Orders</NavLink>
-          </Nav>
+      <Nav>
+        <NavLink href="/admin" prefetch={false}>Admin Dashboard</NavLink>
+        <NavLink href="/">Home</NavLink>
+        <NavLink href="/products">Products</NavLink>
+        <NavLink href="/orders">My Orders</NavLink>
+      </Nav>
       <div className="container my-6">{children}</div>
     </>
   )
